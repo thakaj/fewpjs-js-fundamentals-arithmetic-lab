c4bb155ef0e1a57dddea02866fbd4c8efa3d5901
thakaj <taulant.hakaj@scranton.edu>
2021-04-21 13:23:15 -0400

    Done with assignment

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1 = 31
+let num2 = 2
+var multiply = num1 * num2
 
-// Write your code here
+var random = Math.floor(Math.random()) + 1;
+
+let num3 = 20
+let num4 = 8
+
+var mod = num3 % num4
+
+let max = Math.max(1,20)
